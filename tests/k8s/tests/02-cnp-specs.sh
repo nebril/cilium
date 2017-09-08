56d073a05841ced247fcc9cbe6932f8fcdc9638f
Ian Vernon <ian@covalent.io>
2017-09-08 16:26:20 -0700

    tests: add more logs to 02-cnp-specs
    Signed-off by: Ian Vernon <ian@cilium.io>

--- tests/k8s/tests/02-cnp-specs.sh
+++ tests/k8s/tests/02-cnp-specs.sh
@@ -43,6 +43,7 @@ LOCAL_CILIUM_POD="$(kubectl get pods -n kube-system -o wide | grep $(hostname) |
 log "running test: $TEST_NAME"
 
 function finish_test {
+  log "starting finish_test for ${TEST_NAME}"
   gather_files ${TEST_NAME} k8s-tests
   gather_k8s_logs "2" ${LOGS_DIR}
   log "finished running test: $TEST_NAME"
@@ -50,15 +51,19 @@ function finish_test {
 
 trap finish_test exit
 
+log "creating all resources in ${bookinfo_dir}"
 kubectl create -f "${bookinfo_dir}"
 
+log "getting all pods"
 kubectl get pods -o wide
 
+log "waiting for all K8s pods to be running"
 wait_for_running_pod details-v1
 wait_for_running_pod ratings-v1
 wait_for_running_pod reviews-v1
 wait_for_running_pod reviews-v2
 wait_for_running_pod productpage-v1
+log "done waiting for all K8s pods to be running"
 
 wait_for_service_endpoints_ready default details 9080
 
@@ -88,30 +93,34 @@ set +e
 
 reviews_pod_v1=$(kubectl get pods | grep reviews-v1 | awk '{print $1}')
 
+log "trying to reach ratings:9080/health from reviews pod (should work)"
 kubectl exec -t ${reviews_pod_v1} wget -- --tries=5 ratings:9080/health
 
 if [ $? -ne 0 ]; then abort "Error: could not connect from reviews-v1 to ratings:9080/health service" ; fi
 
+log "trying to reach ratings:9080 from reviews (should work)"
 kubectl exec -t ${reviews_pod_v1} wget -- --tries=5 ratings:9080
 
 if [ $? -ne 0 ]; then abort "Error: could not connect from reviews-v1 to ratings:9080 service" ; fi
 
-
 productpage_v1=$(kubectl get pods | grep productpage-v1 | awk '{print $1}')
 
+log "trying to access details:9080/health from productpage (should work)"
 kubectl exec -t ${productpage_v1} wget -- --tries=5 details:9080/health
 
 if [ $? -ne 0 ]; then abort "Error: could not connect from productpage-v1 to details:9080/health service" ; fi
 
+log "trying to access details:9080 from productpage (should work)"
 kubectl exec -t ${productpage_v1} wget -- --tries=5 details:9080
 
 if [ $? -ne 0 ]; then abort "Error: could not connect from productpage-v1 to details:9080 service" ; fi
 
-
+log "trying to ratings:9080/health from productpage (should work)"
 kubectl exec -t ${productpage_v1} wget -- --tries=5 ratings:9080/health
 
 if [ $? -ne 0 ]; then abort "Error: could not connect from productpage-v1 to ratings:9080/health service" ; fi
 
+log "trying to reach ratings:9080 from productpage (should work)"
 kubectl exec -t ${productpage_v1} wget -- --tries=5 ratings:9080
 
 if [ $? -ne 0 ]; then abort "Error: could not connect from productpage-v1 to ratings:9080 service" ; fi
@@ -123,18 +132,22 @@ cilium_id=$(docker ps -aq --filter=name=cilium-agent)
 # This cilium network policy v2 will work in k8s >= 1.7.x with CRD and v1 with
 # TPR in k8s < 1.7.0
 if [[ "${k8s_version}" == 1.7.* ]]; then
+    log "k8s version is 1.7; adding policies"
     k8s_apply_policy kube-system create "${bookinfo_dir}/policies/cnp.yaml"
 
     if [ $? -ne 0 ]; then abort "policies were not inserted in kubernetes" ; fi
 
+    log "checking that multi-rules policy was added in Cilium"
     docker exec -i ${cilium_id} cilium policy get io.cilium.k8s-policy-name=multi-rules 1>/dev/null
 
     if [ $? -ne 0 ]; then abort "multi-rules policy not in cilium" ; fi
 else
+    log "k8s version is 1.6; adding policies"
     k8s_apply_policy kube-system create "${bookinfo_dir}/policies/cnp-deprecated.yaml"
 
     if [ $? -ne 0 ]; then abort "policies were not inserted in kubernetes" ; fi
 
+    log "checking that multi-rules-deprecated policy was added in Cilium"
     docker exec -i ${cilium_id} cilium policy get io.cilium.k8s-policy-name=multi-rules-deprecated 1>/dev/null
 
     if [ $? -ne 0 ]; then abort "multi-rules-deprecated policy not in cilium" ; fi
@@ -144,46 +157,53 @@ fi
 
 reviews_pod_v1=$(kubectl get pods | grep reviews-v1 | awk '{print $1}')
 
+log "checking that reviews can reach ratings:9080/health (should work)"
 kubectl exec -t ${reviews_pod_v1} wget -- --tries=5 ratings:9080/health
 
 if [ $? -ne 0 ]; then abort "Error: could not connect from reviews-v1 to ratings:9080/health service" ; fi
 
+log "checking that reviews can't reach ratings:9080 (shouldn't work)"
 kubectl exec -t ${reviews_pod_v1} wget -- --connect-timeout=10 --tries=2 ratings:9080
 
 if [ $? -eq 0 ]; then abort "Error: unexpected success from reviews-v1 to ratings:9080 service" ; fi
 
-
 # Productpage should reach every page from Details.
 productpage_v1=$(kubectl get pods | grep productpage-v1 | awk '{print $1}')
 
+log "checking that productpage can reach details:9080/health (should work)"
 kubectl exec -t ${productpage_v1} wget -- --tries=5 details:9080/health
 
 if [ $? -ne 0 ]; then abort "Error: could not connect from productpage-v1 to details:9080/health service" ; fi
 
+log "checking that productpage can reach details:9080 (should work)"
 kubectl exec -t ${productpage_v1} wget -- --tries=5 details:9080
 
 if [ $? -ne 0 ]; then abort "Error: could not connect from productpage-v1 to details:9080 service" ; fi
 
 # But it should fail while reaching out Ratings
+log "checking that productpage can't reach ratings:9080/health (should work)"
 kubectl exec -t ${productpage_v1} wget -- --connect-timeout=10 --tries=2 ratings:9080/health
 
 if [ $? -eq 0 ]; then abort "Error: unexpected success from productpage-v1 to ratings:9080/health service" ; fi
 
+log "checking that productpage can't reach ratings:9080 (shouldn't work)"
 kubectl exec -t ${productpage_v1} wget -- --connect-timeout=10 --tries=2 ratings:9080
 
 if [ $? -eq 0 ]; then abort "Error: unexpected success from productpage-v1 to ratings:9080 service" ; fi
 
-
-echo "SUCCESS!"
+echo "${TEST_NAME} succeeded"
 
 set +e
 
+log "deleting all resources in ${bookinfo_dir}"
 k8s_apply_policy $NAMESPACE delete "${bookinfo_dir}/"
 
 echo "Policies not found error is expected"
 
+log "deleting all policies in ${bookinfo_dir}/policies"
 k8s_apply_policy $NAMESPACE delete "${bookinfo_dir}/policies"
 
+log "checking that all policies were deleted in Cilium"
 docker exec -i ${cilium_id} cilium policy get io.cilium.k8s-policy-name=multi-rules 2>/dev/null
 
 if [ $? -eq 0 ]; then abort "multi-rules policy found in cilium; policy should have been deleted" ; fi
