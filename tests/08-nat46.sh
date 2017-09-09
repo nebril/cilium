d83177aa41ca7c4e2f9d7c6590ac10fda242621a
Ian Vernon <ian@covalent.io>
2017-09-09 09:54:52 -0700

    tests: add extensive logs to 08-nat46
    Signed-off by: Ian Vernon <ian@cilium.io>

--- tests/08-nat46.sh
+++ tests/08-nat46.sh
@@ -12,14 +12,18 @@ redirect_debug_logs ${LOGS_DIR}
 set -ex
 
 function cleanup {
+  log "beginning cleanup for ${TEST_NAME}"
   cilium policy delete --all 2> /dev/null || true
   docker rm -f server client 2> /dev/null || true
   monitor_stop
+  log "finished cleanup for ${TEST_NAME}"
 }
 
 function finish_test {
+  log "beginning finish_test for ${TEST_NAME}"
   gather_files ${TEST_NAME} ${TEST_SUITE}
   cleanup
+  log "done with finish_test for ${TEST_NAME}"
 }
 
 trap finish_test EXIT
@@ -34,8 +38,10 @@ logs_clear
 
 create_cilium_docker_network
 
+log "starting containers"
 docker run -d -i --net=$TEST_NET --name server -l $SERVER_LABEL $NETPERF_IMAGE
 docker run -d -i --net=$TEST_NET --name client -l $CLIENT_LABEL $NETPERF_IMAGE
+log "done starting containers"
 
 CLIENT_IP=$(docker inspect --format '{{ .NetworkSettings.Networks.cilium.GlobalIPv6Address }}' client)
 CLIENT_IP4=$(docker inspect --format '{{ .NetworkSettings.Networks.cilium.IPAddress }}' client)
@@ -44,12 +50,12 @@ SERVER_IP=$(docker inspect --format '{{ .NetworkSettings.Networks.cilium.GlobalI
 SERVER_IP4=$(docker inspect --format '{{ .NetworkSettings.Networks.cilium.IPAddress }}' server)
 SERVER_ID=$(cilium endpoint list | grep $SERVER_IP | awk '{ print $1}')
 
-echo CLIENT_IP=$CLIENT_IP
-echo CLIENT_IP4=$CLIENT_IP4
-echo CLIENT_ID=$CLIENT_ID
-echo SERVER_IP=$SERVER_IP
-echo SERVER_IP4=$SERVER_IP4
-echo SERVER_ID=$SERVER_ID
+log "CLIENT_IP=$CLIENT_IP"
+log "CLIENT_IP4=$CLIENT_IP4"
+log "CLIENT_ID=$CLIENT_ID"
+log "SERVER_IP=$SERVER_IP"
+log "SERVER_IP4=$SERVER_IP4"
+log "SERVER_ID=$SERVER_ID"
 
 wait_for_docker_ipv6_addr client
 wait_for_docker_ipv6_addr server
@@ -65,16 +71,24 @@ cat <<EOF | policy_import_and_wait -
 }]
 EOF
 
+log "updating client endpoint configuration: NAT46=true"
 cilium endpoint config ${CLIENT_ID} NAT46=true
+log "updating server endpoint configuration: NAT46=true"
 cilium endpoint config ${SERVER_ID} NAT46=true
 
 function connectivity_test64() {
+  log "beginning connectivity_test64"
   # ICMPv4 echo request from client to server should succeed
   monitor_clear
+  log "pinging NAT64 address of client from host (should work)" 
   docker exec -i client ping6 -c 10 ::FFFF:$SERVER_IP4 || {
     abort "Error: Could not ping nat64 address of client from host"
   }
+  log "finished connectivity_test64"
 }
 
 connectivity_test64
+log "deleting all policies from Cilium"
 cilium -D policy delete --all
+
+test_succeeded "${TEST_NAME}"
