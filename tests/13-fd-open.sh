4f1dfcbb61a2fe15db62267d34d630ab5c14c580
Ian Vernon <ian@covalent.io>
2017-09-09 09:56:52 -0700

    tests: use test_succeeded function in 13-fd-open
    Signed-off by: Ian Vernon <ian@cilium.io>

--- tests/13-fd-open.sh
+++ tests/13-fd-open.sh
@@ -18,3 +18,5 @@ fd_open=$(sudo lsof 2>/dev/null | grep cilium | wc -l)
 if [[ ${fd_open} -gt ${threshold} ]]; then
     abort "Number of fd open seems abnormal: ${fd_open} (expecting less than ${threshold})"
 fi
+
+test_succeeded "${TEST_NAME}"
