e1316970d1770acae3cdff9f3234e31b1bffe4de
Maciej Kwiek <maciej@isovalent.com>
2020-03-30 17:05:42 +0200

    Revert "ci: skip transparent encryption and direct routing check on 4.19.57"
    This reverts commit 28f8cadca790692453cbb44c8b122926be9975ba.
    
    Signed-off-by: Maciej Kwiek <maciej@isovalent.com>

--- test/k8sT/DatapathConfiguration.go
+++ test/k8sT/DatapathConfiguration.go
@@ -15,7 +15,6 @@
 package k8sTest
 
 import (
-	"context"
 	"fmt"
 	"io/ioutil"
 	"regexp"
@@ -400,7 +399,6 @@ var _ = Describe("K8sDatapathConfig", func() {
 			SkipIfIntegration(helpers.CIIntegrationFlannel)
 			SkipIfIntegration(helpers.CIIntegrationGKE)
 			SkipItIfNoKubeProxy()
-			SkipIfKernel(context.TODO(), kubectl, "4.19.57")
 
 			privateIface, err := kubectl.GetPrivateIface()
 			Expect(err).Should(BeNil(), "Unable to determine private iface")
